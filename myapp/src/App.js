57c485655e1054edac55914b95a807f1add87cf0
sayantan <rautsayantan2407@gmail.com>
2024-07-16 10:13:18 -0700

    added sql converter component

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import GraphContainer from './GraphContainer';
 import ChatBot from './ChatBot';
+import SQLConverter from './SqlConverter';
 import './App.css';
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
       </div>
       <div id="graphdiv" className="centeredElement">
           <GraphContainer />
+      </div>
+      <div id="converterdiv">
+          <SQLConverter />
       </div>
     </div>
   );
---
+++ myapp/src/SqlConverter.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { getSQLQuery } from './openaiService';
+
+const SQLConverter = () => {
+  const [text, setText] = useState('');
+  const [sqlQuery, setSqlQuery] = useState('');
+
+  const handleConvert = async () => {
+    const query = await getSQLQuery(text);
+    setSqlQuery(query);
+  };
+
+  return (
+    <div>
+      <h1>Natural Language to SQL Converter</h1>
+      <textarea
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        rows='5'
+        cols='50'
+        placeholder='Enter your text here'
+      />
+      <br />
+      <button onClick={handleConvert}>Convert to SQL</button>
+      {sqlQuery && (
+        <div>
+          <h2>Generated SQL Query</h2>
+          <pre>{sqlQuery}</pre>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SQLConverter;
