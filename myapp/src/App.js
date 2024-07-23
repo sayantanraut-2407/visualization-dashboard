af67d6d92f51fe679371d29fbf795d0d59a3889c
Sayantan Raut <rautsayantan2407@gmail.com>
2024-07-23 10:30:33 -0700

    added click event

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GraphContainer from './GraphContainer';
 import ChatBot from './ChatBot';
 import SQLConverter from './SqlConverter';
@@ -12,6 +12,9 @@ import imagePath from './resources/lab_logo.png';
 import vidPath from './resources/bg_vid.mp4';
 
 function App() {
+
+  const [isGraphCalled, setIsGraphCalled] = useState(false);
+
   return (
     <div className="App" id="maindiv">
       <div className="video-background">
@@ -20,7 +23,6 @@ function App() {
         </video>
         <h1 className="headerElement">Data Visualization App</h1>
         <img src={imagePath} alt="Fixed" className="fixed-image" />
-        <h3 className='siteDescription'>Web application for displaying visualizations on sarcoma samples</h3>
         {/* <div id="chatbotdiv" className="chatBotElement">
           <ChatBot />
         </div> */}
@@ -28,17 +30,18 @@ function App() {
           <Button color="secondary" 
             onClick={() => {
               console.log("event logged")
+              setIsGraphCalled(true)
             }}>
             Begin Visualization
           </Button>
         </div>
         <div id="converterdiv" className='converterElement'>
-          <div className='sqlDiv'>
-              {/* <SQLConverter /> */}
-          </div>
-          <div id="graphdiv" className="centeredElement">
+          {/* <div className='sqlDiv'>
+              <SQLConverter />
+          </div> */}
+          {isGraphCalled && (<div id="graphdiv" className="centeredElement">
               <GraphContainer />
-          </div>
+          </div>)}
         </div>
       </div>
     </div>
